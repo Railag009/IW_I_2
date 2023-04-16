82d7a239c8e1ab16098843cefeafeb600e91b529
Railag009 <Solovevkirill26072002@ya.ru>
2023-04-16 21:37:28 +0300

    Добавьте файлы проекта.

---
+++ dos/dos.cpp
@@ -0,0 +1,115 @@
+﻿// Дано два списка получить новый список, состоящий из элементов, принадлежащих интервалу [a, b].
+//
+
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct list
+{
+	int info;
+	list* next;
+	list* prev;
+};
+
+int main()
+{
+
+	cout << "Enter first list" << endl;
+
+	list* head1, * p1, * r1;
+	list* head2, * p2, * r2;
+	list* head, * p, * r;
+	int x, y;
+	head = new list;
+	head1 = new list;
+	head2 = new list;
+	cin >> x;
+	head->info = x;
+	head->next = NULL;
+	head->prev = NULL;
+	head1->next = NULL;
+	head1->prev = NULL;
+	p = head;
+	p1 = head1;
+	p2 = head2;
+
+	cin >> x;
+	while (x)
+	{
+		r1 = new list;
+		r1->info = x;
+		r1->next = NULL;
+		r1->prev = p1;
+		p1->next = r1;
+		p1 = r1;
+		cin >> x;
+
+	}
+
+	cout << "Enter second list" << endl;
+
+	cin >> x;
+	while (x)
+	{
+		r2 = new list;
+		r2->info = x;
+		r2->next = NULL;
+		r2->prev = p2;
+		p2->next = r2;
+		p2 = r2;
+		cin >> x;
+
+	}
+
+	int a, b;
+	cout << "Enter left interval - ";
+	cin >> a;
+	cout << "Enter right interval - ";
+	cin >> b;
+
+	p1 = head1;
+	p2 = head2;
+
+	while (p1 != NULL)
+	{
+
+		if (p1->info >= a && p1->info <= b)
+		{
+
+			r = new list;
+			r->info = p1->info;
+			r->next = NULL;
+			r->prev = p;
+			p->next = r;
+			p = r;
+			cout << p->info << setw(3);
+
+		}
+
+		p1 = p1->next;
+
+	}
+
+	while (p2 != NULL)
+	{
+
+		if (p2->info >= a && p2->info <= b)
+		{
+
+			r = new list;
+			r->info = p2->info;
+			r->next = NULL;
+			r->prev = p;
+			p->next = r;
+			p = r;
+			cout << p->info << setw(3);
+
+		}
+
+		p2 = p2->next;
+
+	}
+
+}
---
+++ uno/uno.cpp
@@ -0,0 +1,153 @@
+﻿// Дан двунаправленный список. Если в нем есть n-чисел, у которых есть хотя бы 2 нечетных делителя, то удалить все числа,
+// перед которыми расположенные соверщенные, иначе вставить между равными n.
+
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct list
+{
+	int info;
+	list* next;
+	list* prev;
+};
+
+int sum(int a, int b)
+{
+
+	if (b > a) return 0;
+	if (a % b == 0 && b % 2 != 0) return sum(a, b + 1) + 1;
+	return sum(a, b + 1);
+
+}
+int sov(int a, int b)
+{
+
+	if (b == a) return 0;
+	if (a % b == 0) return sov(a, b + 1) + b;
+	return sov(a, b + 1);
+
+}
+
+
+int main()
+{
+
+	list* head, * p, * r;
+	int x, y;
+	head = new list;
+	cin >> x;
+	head->info = x;
+	head->next = NULL;
+	head->prev = NULL;
+	p = head;
+
+	cin >> x;
+	while (x)
+	{
+		r = new list;
+		r->info = x;
+		r->next = NULL;
+		r->prev = p;
+		p->next = r;
+		p = r;
+		cin >> x;
+
+	}
+
+	int n;
+	cout << "Enter number - ";
+	cin >> n;
+
+	int i = 0;
+
+	p = head;
+
+	while (p != NULL)
+	{
+
+		int a = sum(p->info, 1);
+		if (a > 1) i++;
+
+		p = p->next;
+
+	}
+
+	if (i == n)
+	{
+
+		p = head->next;
+
+		while (p != NULL)
+		{
+
+			if (p->prev->info == sov(p->prev->info, 1))
+			{
+
+				r = p;
+				p->prev->next = r->next;
+				p->next->prev = p->prev;
+				p = p->next;
+				delete r;
+				p = p->next;
+
+
+			}
+			else
+			{
+
+				p = p->next;
+
+			}
+
+		}
+
+		p = head;
+
+		while (p != NULL)
+		{
+
+			cout << p->info << setw(3);
+			p = p->next;
+
+		}
+
+	}
+	else
+	{
+
+		p = head;
+
+		while (p->next != NULL)
+		{
+
+			if (p->next->info == p->info)
+			{
+
+				r = new list;
+				r->info = n;
+				r->next = p->next;
+				r->prev = p;
+				p->next = r;
+				p = r;
+				
+			}
+
+			p = p->next;
+			
+		}
+
+		p = head;
+
+		while (p != NULL)
+		{
+
+			cout << p->info << setw(3);
+			p = p->next;
+
+		}
+
+	}
+
+}
